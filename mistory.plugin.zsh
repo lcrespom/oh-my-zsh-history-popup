3d1926e3429fc0545c2e4cb548b3fd2ae841d550
Luis Crespo <lcrespo@es.ibm.com>
2019-12-01 22:29:27 +0100

    Almost fully working

--- mistory.plugin.zsh
+++ mistory.plugin.zsh
@@ -4,7 +4,10 @@
 # - [x] Get content of current line as history filter => $BUFFER
 # - [x] Disable error message on "which" => was silly bug
 # - [x] Gather history
-# - [ ] Show in popup
+# - [x] Show in popup
+
+# Regex for matching and splitting a history line in line number and history text.
+_MISTORY_LINE_PATTERN='^[ ]*([0-9]+)\*?[ ]+(.+)$'
 
 
 _mistory_check_dialog() {
@@ -26,67 +29,43 @@ _mistory_dialog() {
 }
 
 _mistory_main() {
+  # Check if `dialog` command is available, exit otherwise
   _mistory_check_dialog || return
+
   local history_array dialog_items
+  # Get history lines matching text in prompt into an array
   # Multiline to array: https://unix.stackexchange.com/a/29748
   history_array=("${(@f)$(history | grep "$BUFFER")}")
+  #TODO if error / array is empty, make error noise and exit
+
+  # Build array of history line pairs (number, text)
   dialog_items=()
   for history_line in $history_array
   do
-    [[ $history_line =~ '^[ ]*([0-9]+)[ ]+(.+)$' ]] && \
+    [[ $history_line =~ $_MISTORY_LINE_PATTERN ]] && \
       dialog_items+=($match[1]) && dialog_items+=($match[2])
   done
-  _mistory_dialog $dialog_items[-2] $dialog_items
 
+  # Show dialog :-)
+  local tmpfile=$(mktemp)
+  _mistory_dialog $dialog_items[-2] $dialog_items 2> $tmpfile
+  local dialog_return=$?
+  local sel_number=$(cat $tmpfile)
+  rm $tmpfile
+  #TODO trap "rm -f $tempfile" 0 0 1 2 3 15
+
+  # Display selection
   zle kill-whole-line
-  #zle -U "You were typing <$wastyping>, right?"
-  #zle accept-line
+  zle accept-line
+  #TODO if user did not press < OK >, exit but re-display buffer
+  [[ $dialog_return -ne 0 ]] && return
+  # Get history line from selected menu item number
+  local sel_line=$(history $sel_number $sel_number)
+  # Get only the text from the history line
+  [[ $sel_line =~ $_MISTORY_LINE_PATTERN ]] && \
+    zle -U $match[2]
 }
 
 
 zle -N _mistory_main
 bindkey "^[[5~" _mistory_main
-
-
-#----- Testing stuff -----
-
-_mistory_split_history_line() {
-  match=""
-  echo "Testing <$1>"
-  [[ $1 =~ '^[ ]*([0-9]+)[ ]+(.+)$' ]]
-  echo "Num: <$match[1]>"
-  echo "Text: <$match[2]>"
-}
-
-mistory_array_manipulation() {
-  # Define array
-  a=(pim pam pum haha)
-  # Add element to array
-  a+=(bum)
-  # Loop array
-  for e in $a; do echo $e; done
-  # Index from start
-  echo $a[1]
-  # Index from end
-  echo $a[-1]
-}
-
-mistory_test() {
-  local history_array dialog_items
-  history_array=("${(@f)$(history | grep dialog)}")
-  #echo $history_array
-  dialog_items=()
-  for history_line in $history_array
-  do
-    [[ $history_line =~ '^[ ]*([0-9]+)[ ]+(.+)$' ]] && \
-      dialog_items+=($match[1]) && dialog_items+=($match[2])
-  done
-  _mistory_dialog $dialog_items[-2] $dialog_items
-  #for item in $dialog_items; do echo $item; done
-  #echo $dialog_items
-  # local line result
-  # line="  560  git commit -m \"Initial version\""
-  # _mistory_split_history_line $line
-}
-
-
